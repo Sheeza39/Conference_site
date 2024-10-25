4c112f9f33cb2babc7198c956a73385fe66578d3
Sheeza39 <your.email@sheezamushtaq0309@gmail.com>
2024-10-25 23:43:22 +0500

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// script.js
+
 const hamburgerBtn = document.getElementById('hamburger-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 
 hamburgerBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const moreButton = document.querySelector('.more-button button');
+    const hiddenSpeaker = document.querySelector('.speakers.hidden');
+
+    moreButton.addEventListener('click', function () {
+        if (hiddenSpeaker) {
+            hiddenSpeaker.classList.remove('hidden'); 
+            moreButton.style.display = 'none'; 
+        }
+    });
 });
